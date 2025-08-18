0813a246d7d17de910f98ebb42126e9bda189557
ArihantMarwaha <65076729+ArihantMarwaha@users.noreply.github.com>
2025-08-18 13:42:41 +0530

    Add files via upload

---
+++ blank/week1.playground/Pages/practice.xcplaygroundpage/Contents.swift
@@ -0,0 +1,17 @@
+import Foundation
+var ari : Int
+var bol : String
+var hop : Bool
+let pi : Float
+var tan : Double
+
+ari = 8
+bol = "Arihant"
+hop = true
+pi = 3.14
+tan = 3.3344
+
+var conv = Double(ari) //convertion of data
+
+
+
